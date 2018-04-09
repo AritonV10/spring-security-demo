48b085ce595b1b9d0b54da5d20054e63fc79b480
Ariton Vio <33898905+AritonV10@users.noreply.github.com>
2018-04-09 23:33:35 +0300

    Create ControllersConfig.java

---
+++ src/main/java/com/vio/spring5/springsecuritydemo/config/ControllersConfig.java
@@ -0,0 +1,35 @@
+package com.vio.spring5.springsecuritydemo.config;
+
+import org.springframework.context.MessageSource;
+
+@Configuration
+@ComponentScan(basePackage = {"com.vio.spring5.springsecuritydemo.mvc"}
+@EnableWebMvc
+public class ControllersConfig extends WebConfigurerAdapter{
+
+    
+    /**
+     * @{MessageSource} bean to use different languages for validations
+     */
+    @Bean
+    public MessageSource messageSource() {
+      ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+      messageSource.setBasename("classpath:/messages/messages");
+      messageSource.setDefaultEncoding("UTF-8");
+		
+		return messageSource;
+	}
+  
+   @Override
+   public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+  }
+  
+   @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/**")
+          .addResourceLocations("/", "/resources/");
+  }
+  
+}
