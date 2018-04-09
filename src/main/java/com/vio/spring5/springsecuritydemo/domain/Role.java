61e35f2ecd5ae478fa7fff160339a89413463575
Ariton Vio <33898905+AritonV10@users.noreply.github.com>
2018-04-09 21:04:02 +0300

    Role object

---
+++ src/main/java/com/vio/spring5/springsecuritydemo/domain/Role.java
@@ -0,0 +1,57 @@
+package com.vio.spring5.springsecuritydemo.domain;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @{User} role(s) which will be set before persisting the new @{User} object
+ * Using @{lombok} for getter, setters and equals
+ */
+
+@Entity
+@Getter @Setter
+@HashAndEqualsCode
+public class Role {
+
+
+  /**
+   * @{Role} ID
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  
+  /**
+   * @{Role} name
+   */
+  private String name;
+  
+  /*
+   * @ManyToMany relationship with @{User}
+   * 
+   */
+  @ManyToMany(mapped by = "roles")
+  private Collection<User> user;
+  
+  
+  /*
+   * Empty @{Role} constructor
+   */
+  public Roles() { super(): }
+  
+  /*
+   * @{Role} constructor
+   * @param name @{Role} name
+   */
+  public Roles(String name) { super(); this.name = name; }
+  
+}
