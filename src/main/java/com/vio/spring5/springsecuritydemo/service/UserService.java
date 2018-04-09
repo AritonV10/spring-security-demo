e0b669468dc49eaa17bf47a9f31c34f984a8b89e
Ariton Vio <33898905+AritonV10@users.noreply.github.com>
2018-04-09 21:36:41 +0300

    UserService

---
+++ src/main/java/com/vio/spring5/springsecuritydemo/service/UserService.java
@@ -0,0 +1,28 @@
+
+package com.vio.spring5.springsecuritydemo.service;
+
+import com.vio.spring5.springsecuritydemo.domain.User;
+
+/*
+ * @{User} service layer
+ */
+public interface UserService {
+  
+  /**
+   * We will be expecting an @{UserCommand} object, set It's details to a new @{User} domain object
+   * and persist the newly created @{User} then return it
+   * Here you can @throw an @{EmailAlreadyExists} exception and @{UsernameAlreadyExists} exception
+   * I will not do that in this demo. I'd highly suggest you to do it.
+   * @param command @{UserCommand} object
+   * @return @{User} domain object
+   */
+  User registerUser(UserCommand command);
+  
+  /**
+   * Find a @{User} by their username and return it
+   * You can @throw an @{AccountDoesntExist} exception
+   * @param username @{User} username
+   * @return @{User} domain object
+   */
+  User getUserByUsername(String username);
+}
