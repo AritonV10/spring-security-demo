96a2e8e330067cc105a7686b607dca830648dad6
Ariton Vio <33898905+AritonV10@users.noreply.github.com>
2018-04-09 20:53:41 +0300

    User object

--- src/main/java/com/vio/spring5/springsecuritydemo/domain/User.java
+++ src/main/java/com/vio/spring5/springsecuritydemo/domain/User.java
@@ -1,1 +1,70 @@
+import package com.vio.spring5.springsecuritydemo.domain
 
+import java.util.Collection;
+
+import javax.persistence.JoinColumn;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+
+/**
+* This is just a simple domain object. You can extend and build on top of it.
+* The domain object we will use to persist a new user and logging in.
+* I will be using @{lombok} throughout the demo to generate the getter and setters automatically
+*/
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode
+public class User {
+  
+  /**
+   * The @{User} ID generated AUTO by the database
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  
+  /**
+   * @{User} username
+   */
+  private String username;
+  /**
+   * @{User} password
+   */
+  private String password;
+  
+  /**
+   * @{User} email
+   */
+  private String email;
+  
+  /**
+  * @{User} roles (authorities)
+  * @ManyToMany relationship with @{Roles}
+  * Since It's a @ManyToMany relationship, 
+  * we use the @JoinColumn to join the @{User} id and @{Role} id to one table
+  */
+  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinColumn(name = "user_role",
+              joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+              inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+  private Collection<Role> roles;
+  
+  /**
+  * Since this is just a simple demo, it will be set to true after registration
+  * You can initialize as false after the registration if you want the
+  * @{User} to confirm their Email first. Then set it to true and persist it to the database
+  */
+  private isEnabled;
+
+}
